ba211100586266229a03f7837b232625a438161c
Juan Caballero Fernandez <juan_fernandez@nuance.com>
2021-10-25 10:47:14 +0200

    Importing Tabulator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {TabulatorFull as Tabulator} from 'tabulator-tables';
+
+//define some sample data
+var tabledata = [
+  {id:1, name:"Oli Bob", age:"12", col:"red", dob:""},
+  {id:2, name:"Mary May", age:"1", col:"blue", dob:"14/05/1982"},
+  {id:3, name:"Christine Lobowski", age:"42", col:"green", dob:"22/05/1982"},
+  {id:4, name:"Brendon Philips", age:"125", col:"orange", dob:"01/08/1980"},
+  {id:5, name:"Margret Marmajuke", age:"16", col:"yellow", dob:"31/01/1999"},
+];
+
+//create Tabulator on DOM element with id "example-table"
+var table = new Tabulator("#example-table", {
+  height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+  data:tabledata, //assign data to table
+  layout:"fitColumns", //fit columns to width of table (optional)
+  columns:[ //Define Table Columns
+    {title:"Name", field:"name", width:150},
+    {title:"Age", field:"age", hozAlign:"left", formatter:"progress"},
+    {title:"Favourite Color", field:"col"},
+    {title:"Date Of Birth", field:"dob", sorter:"date", hozAlign:"center"},
+  ],
+  rowClick:function(e, row){ //trigger an alert message when the row is clicked
+    alert("Row " + row.getData().id + " Clicked!!!!");
+  },
+});
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +46,7 @@ function App() {
         </a>
       </header>
     </div>
+    <div id="example-table"></div>
   );
 }
 
