7d784ac5cd113b0be6a4492107a39a798220bf08
Juan Caballero Fernandez <juan_fernandez@nuance.com>
2021-10-25 09:33:59 +0200

    Testing the GitHub actions and the deployment of the app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning React in GitHub Pages
+          Learning React in GitHub Pages deployed with GitHub actions
         </a>
       </header>
     </div>
